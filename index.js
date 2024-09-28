00764c29a6fad9d0a5f4bae01b5096305950ad73
lepeha <nostresko@gmail.com>
2024-09-28 12:45:09 +0400

    walidator

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
+import UserModel from './Models/User.js';
+import bcrypt from 'bcrypt';
+
+const URL = 'mongodb+srv://lepeha:Pass123@cluster0myself.ij4cz.mongodb.net/'
 
 mongoose
-  .connect('mongodb+srv://lepeha:Pass123@cluster0myself.ij4cz.mongodb.net/')
+  .connect(URL)
   .then(() => console.log('DB OK'))
   .catch((err) => console.log('DB errror', err));
  
@@ -11,8 +17,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {
-  
+app.post('/auth/register', registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400),json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10); // salt это алгоритм шифрования пароля
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
