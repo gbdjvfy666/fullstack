f04bfa79fd9c3c459b08a447fb1d6fc4e1755edb
lepeha <nostresko@gmail.com>
2024-09-28 17:48:00 +0400

    1

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,25 +1,23 @@
-// функция посредник можно ли возвращать секретную инфу или нет
 import jwt from "jsonwebtoken"; 
 
 export default (req, res, next) => {
   const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
 
   if (token) {
-      try {
-        const decoded = jwt.verify(token, 'secret123');
-
-        req.userId = decoded._id;
-        next()
-      } catch(e) {
-        return res.status(403).json({
-          message: 'Нет доступа'
-        });
-      }
+    try {
+      const decoded = jwt.verify(token, 'secret123');
+      req.userId = decoded._id;
+      next();  // Передача управления следующей middleware-функции
+    } catch (e) {
+      return res.status(403).json({
+        message: 'Нет доступа'
+      });
+    }
   } else {
-     return res.status(403).json({
+    return res.status(403).json({
       message: 'Нет доступа!'
     });
   }
-  res.send(token);
-  
-}
+
+  // Убираем лишний res.send(token);
+};
