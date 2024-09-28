bcac9d064fb3f4eac694811d2f2de65923635a4b
lepeha <nostresko@gmail.com>
2024-09-28 14:48:52 +0400

    login

--- index.js
+++ index.js
@@ -23,18 +23,40 @@ app.post('/auth/login', async (req, res) => {
     const user = await UserModel.findOne({email: req.body.email});
 
     if(!user) {
-      return req.status(404).json({
+      return res.status(404).json({
         message: 'Пользователь не найден'
       });
     }
 
     const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
     if (!isValidPass) {
-      return req.status(404).json({
+      return res.status(400).json({
       message: 'Неверный логин или пароль',
     });
   }
-  } catch (err) {}
+
+  const token = jwt.sign({
+    _id: user._id,
+  }, 
+  'secret123',
+  {
+    expiresIn: '30d',
+  },
+);
+
+const {passwordHash, ...userData} = user._doc;
+
+res.json({
+  ...userData,
+  token,
+});
+
+  }  catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'не получилось авторизоваться'
+    })
+  }
 });
 
 app.post('/auth/register', registerValidation, async (req, res) => {
