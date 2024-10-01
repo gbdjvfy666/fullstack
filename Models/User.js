633f0756bc132c285f72c60c499d640327d13b4b
lepeha <nostresko@gmail.com>
2024-10-01 11:07:14 +0400

    multer

--- Models/User.js
+++ Models/User.js
@@ -5,17 +5,21 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true, 
   },
+  
   email: {
     type: String,
     required: true,
     unique: true,
   },
+  
   passwordHash: {
     type: String,
     require: true,
   },
+  
   avatarUrl: String,
 },  {
+  
   timestamps: true,
 });
 
